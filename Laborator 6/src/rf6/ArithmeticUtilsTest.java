3f578a3e4e4ed3fff868faaf8ecf606cd2217789
Adrian Iuga <adrian.valentin.iuga@gmail.com>
2018-04-19 08:33:17 +0300

    work

---
+++ Laborator 6/src/rf6/ArithmeticUtilsTest.java
@@ -0,0 +1,14 @@
+package rf6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArithmeticUtilsTest {
+
+	@Test
+	void test() {
+		assertEquals(3, Math.abs(-1+4));
+	}
+
+}
